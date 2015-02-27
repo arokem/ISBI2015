a1f7d1ea40825b8eecc490739a7331d9632ba14a
cpoetter <cpoetter@stanford.edu>
2015-02-26 23:39:44 -0800

    Improvements in Parameter Assignment

--- parallelization.py
+++ parallelization.py
@@ -18,7 +18,7 @@ class parallelization():
         self.maximum_number_of_cores = maximum_number_of_cores
         return None
         
-    def worker(self, q, process_number, cpus, function, number_of_voxels, *args):
+    def worker(self, q, process_number, cpus, function, n, args, kwargs):
         done = False 
         while done != True:
             
@@ -27,19 +27,19 @@ class parallelization():
                 self.calculations[process_number] += 1
         
             # Everything done
-            if vox >= number_of_voxels:
+            if vox >= n:
                 done = True
                 break
         
             # Calculate approximate time remaining for fitting
-            percentage_realistic = int(vox*1.0/number_of_voxels*100.0)
+            percentage_realistic = int(vox*1.0/n*100.0)
             
             if percentage_realistic > self.percent.value:
                 self.percent.value = percentage_realistic
 
                 # Three-point estimation (PERT-Estimation)
-                percentage_pessimistic = int(min(self.calculations)*self.number_of_cores*1.0/number_of_voxels*100.0)
-                percentage_optimistic = int(max(self.calculations)*self.number_of_cores*1.0/number_of_voxels*100.0)
+                percentage_pessimistic = int(min(self.calculations)*self.number_of_cores*1.0/n*100.0)
+                percentage_optimistic = int(max(self.calculations)*self.number_of_cores*1.0/n*100.0)
                 
                 if percentage_pessimistic == 0:
                     percentage_pessimistic = 1
@@ -53,44 +53,56 @@ class parallelization():
                 sys.stdout.write('{:3.0f}%  {:8.2f}min remaining\n'.format(percentage_realistic, time_remaining))
     
             params = ()
-            for param in args[0]:
+            for param in args:
                 if len(param) != 1:
                     params = params + (param[vox],)
                 else:
                     params = params + (param[0],)
                     
-            if len(function) != 1:
-                result = function[vox](*params)
+            params_dict = {}
+            for key, value in kwargs.iteritems():
+                if len(value) != 1:
+                    params_dict[key] = value[vox]
+                else:
+                    params_dict[key] = value[0]        
+                    
+                    
+            if isinstance(function, list):
+                result = function[vox](*params, **params_dict)
             else:
-                result = function[0](*params)
+                result = function(*params, **params_dict)
     
             results = []
             results.append(vox)
             results.append(result)
             q.put(results)
         
-    def start(self, function, number_of_voxels, *args):
+    def start(self, function, n, *args, **kwargs):
         """"
         Starts the Parallelization of the Parallelization Class.
         
         Parameters:
         ----------
-        function:           (List) List of functions that should be executed, if always the same 
-                            function should be used, then a list containing only one function [function] has to be assigned.
-        number_of_voxels:   (int) Amount of Processes to be parallelized.
-        (optional) *args:   (Tuple of Lists) Parameter for executed function. ALL PARAMETER MUST BE ASSIGNED, even optional
-                            ones if one optional parameter should be used, in the correct order, without name assignement.
-                            If always the same parameter should be used, then a list containing one parameter [parameter]
-                            has to be assigned.
+        function:             (List) List of functions that should be executed, if always the same 
+                              function should be used, only the function name has to be assigned. 
+                              At iteration i function[i] of the list will be executed, together with the 
+                              belonging list element of args[i].
+        n:                    (int) Number of times the function should be called.
+        (optional) *args:     (Tuple of Lists) Parameter for executed function. If always the same parameter
+                              should be used, then a list containing one parameter [parameter] has to be
+                              assigned. Otherwise args[i] will be passed through to the function.
+        (optional) **kwargs:  (Dictionary of Lists) Optional Keyword Parameter for the function. The Value 
+                              should always contain a list. If always the same parameter should be used,
+                              then a list containing one parameter kwarg=[parameter] has to be assigned. 
                             
         Returns:
         --------
-        List of return values from the excecuted function. The length equals number_of_voxels.
+        List of return values from the excecuted function. The length equals n.
                             
         Example:
         -------
-        models = p.start([TensorModel], 4, [gtab1, gtab2, gtab3, gtab4])
-        fits = p.start([i.fit for i in models], 4, [data1, data2, data3, data4], [TE])
+        models = p.start(TensorModel, 4, [gtab1, gtab2, gtab3, gtab4])
+        fits = p.start([i.fit for i in models], 4, [data1, data2, data3, data4], [TE], sphere=[sphere])
         """
         #Multicore Calculation
         self.number_of_cores = cpu_count()
@@ -109,12 +121,12 @@ class parallelization():
         
         for i in range(self.number_of_cores):
             self.calculations[i] = 0
-            processes.append(Process(target=self.worker, args=(q, i, self.number_of_cores, function, number_of_voxels, args)))
+            processes.append(Process(target=self.worker, args=(q, i, self.number_of_cores, function, n, args, kwargs)))
             processes[-1].start()  
         
-        return_values = [None] * number_of_voxels
+        return_values = [None] * n
         
-        for i in range(number_of_voxels):
+        for i in range(n):
             results = q.get()
             vox = results[0]
             return_values[vox] = results[1]
