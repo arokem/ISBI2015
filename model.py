21c53e0e6ea2b24a101902df7f83f9a91152feec
arokem <arokem@gmail.com>
2015-02-26 19:59:02 -0800

    Add a block to the design matrix, with b-value dependent AD/RD.

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import dipy.reconst.sfm as sfm
+import dipy.reconst.dti as dti
+from dipy.sims import voxel as sims
 from dipy.core.geometry import cart2sphere
 from dipy.reconst.shm import real_sph_harm
 from scipy.special import genlaguerre, gamma
@@ -9,11 +11,11 @@ import scipy.optimize as opt
 import scipy.stats as stats
 
 my_responses = []
-
 for ad in np.arange(0.001, 0.0018, 0.0005):
     for rd in np.arange(0, ad, 0.0005):
         this_response = [ad, rd, rd]
         my_responses.append(this_response)
+
 
 class BiExponentialIsotropicModel(sfm.IsotropicModel):
     def _nlls_err_func(self, parameter, data):
@@ -57,6 +59,7 @@ class BiExponentialIsotropicModel(sfm.IsotropicModel):
         
         return BiExponentialIsotropicFit(self, params, n_vox)
 
+
 class BiExponentialIsotropicFit(sfm.IsotropicFit):
     def predict(self, gtab=None):
         if gtab is None:
@@ -75,95 +78,63 @@ class BiExponentialIsotropicFit(sfm.IsotropicFit):
         return y
 
 
-def sfm_design_matrix(gtab, sphere, responses=my_responses):
-        dm = []
-        for response in responses :
-            dm.append(sfm.sfm_design_matrix(gtab, sphere, response,
-                                            'signal'))
-        return np.concatenate(dm, -1)
-
-
-def _kappa(zeta, n, l):
-    return np.sqrt((2 * factorial(n - l)) / (zeta ** 1.5 * gamma(n + 1.5)))
-
-
-def shore_design_matrix(radial_order, zeta, gtab, tau=1 / (4 * np.pi ** 2)):
-    r"""Compute the SHORE matrix for modified Merlet's 3D-SHORE [1]_
-
-    ..math::
-            :nowrap:
-                \begin{equation}
-                    \textbf{E}(q\textbf{u})=\sum_{l=0, even}^{N_{max}}
-                                            \sum_{n=l}^{(N_{max}+l)/2}
-                                            \sum_{m=-l}^l c_{nlm}
-                                            \phi_{nlm}(q\textbf{u})
-                \end{equation}
-
-    where $\phi_{nlm}$ is
-    ..math::
-            :nowrap:
-                \begin{equation}
-                    \phi_{nlm}^{SHORE}(q\textbf{u})=\Biggl[\dfrac{2(n-l)!}
-                        {\zeta^{3/2} \Gamma(n+3/2)} \Biggr]^{1/2}
-                        \Biggl(\dfrac{q^2}{\zeta}\Biggr)^{l/2}
-                        exp\Biggl(\dfrac{-q^2}{2\zeta}\Biggr)
-                        L^{l+1/2}_{n-l} \Biggl(\dfrac{q^2}{\zeta}\Biggr)
-                        Y_l^m(\textbf{u}).
-                \end{equation}
-
-    Parameters
-    ----------
-    radial_order : unsigned int,
-        an even integer that represent the order of the basis
-    zeta : unsigned int,
-        scale factor
-    gtab : GradientTable,
-        gradient directions and bvalues container class
-    tau : float,
-        diffusion time. By default the value that makes q=sqrt(b).
-
-    References
-    ----------
-    .. [1] Merlet S. et. al, "Continuous diffusion signal, EAP and
-    ODF estimation via Compressive Sensing in diffusion MRI", Medical
-    Image Analysis, 2013.
-
+def response_design_matrix(gtab, sphere, response, p_ad=None, p_rd=None):
+    """
+    A response-function dependent design matrix
     """
     mat_gtab = grad.gradient_table(gtab.bvals[~gtab.b0s_mask],
-                                   gtab.bvecs[~gtab.b0s_mask])
+                                       gtab.bvecs[~gtab.b0s_mask])
+    mat = np.empty((np.sum(~gtab.b0s_mask),
+                        sphere.vertices.shape[0]))
 
-    qvals = np.sqrt(mat_gtab.bvals / (4 * np.pi ** 2 * tau))
-    qvals[mat_gtab.b0s_mask] = 0
-    bvecs = mat_gtab.bvecs
+    # Calculate column-wise:
+    for ii, this_column in enumerate(sphere.vertices):
+        # Rotate the canonical tensor towards this vertex and calculate the
+        # signal you would have gotten in the direction
+        evecs = sims.all_tensor_evecs(this_column)
+        if p_ad is not None and p_rd is not None:
+            # And if you have p_ad and p_rd then also row-wise:
+            for jj, row_bvec in enumerate(mat_gtab.bvecs):
+                row_gtab = grad.gradient_table([mat_gtab.bvals[jj]],
+                                               [row_bvec])
+                this_ad = np.exp(np.polyval(p_ad, row_gtab.bvals[0]))
+                this_rd = np.exp(np.polyval(p_rd, row_gtab.bvals[0]))
+                response = [this_ad, this_rd, this_rd]
+                sig = sims.single_tensor(row_gtab, evals=response,
+                                             evecs=evecs)
+                # For regressors based on the single tensor, remove $e^{-bD}$
+                iso_sig = np.exp(-row_gtab.bvals * np.mean(response))
+                mat[jj, ii] = sig - iso_sig
+        else:
+            sig = sims.single_tensor(mat_gtab, evals=response,
+                                     evecs=evecs)
+            iso_sig = np.exp(-mat_gtab.bvals * np.mean(response))
+            mat[:, ii] = sig - iso_sig
 
-    qgradients = qvals[:, None] * bvecs
+    return mat
 
-    r, theta, phi = cart2sphere(qgradients[:, 0], qgradients[:, 1],
-                                qgradients[:, 2])
-    theta[np.isnan(theta)] = 0
-    F = radial_order / 2
-    n_c = np.round(1 / 6.0 * (F + 1) * (F + 2) * (4 * F + 3))
-    M = np.zeros((r.shape[0], n_c))
 
-    counter = 0
-    for l in range(0, radial_order + 1, 2):
-        for n in range(l, int((radial_order + l) / 2) + 1):
-            for m in range(-l, l + 1):
-                M[:, counter] = real_sph_harm(m, l, theta, phi) * \
-                    genlaguerre(n - l, l + 0.5)(r ** 2 / zeta) * \
-                    np.exp(- r ** 2 / (2.0 * zeta)) * \
-                    _kappa(zeta, n, l) * \
-                    (r ** 2 / zeta) ** (l / 2)
-                counter += 1
-    return M
+def sfm_design_matrix(gtab, sphere, responses=my_responses, p_ad=None,
+                                                            p_rd=None):
+    """
+    This creates the full design matrix out of individual responses functions
+    and from the fit across
+    """
+    dm = []
+    for response in responses :
+        dm.append(response_design_matrix(gtab, sphere, response))
+
+    if p_ad is not None and p_rd is not None:
+        dm.append(response_design_matrix(gtab, sphere, response, p_ad, p_rd))
+    return np.concatenate(dm, -1)
 
 
 class Model(sfm.SparseFascicleModel):
     @sfm.auto_attr
     def design_matrix(self):
         return sfm_design_matrix(self.gtab, self.sphere,
-                                 responses=my_responses)
-        #return shore_design_matrix(40, 2000, self.gtab)
+                                 responses=my_responses, p_ad=self.p_ad,
+                                 p_rd=self.p_rd)
         
     def fit(self, data, TE=None, te_order=3, mask=None):
         """
@@ -188,7 +159,6 @@ class Model(sfm.SparseFascicleModel):
         Fit object
 
         """
-        
         te_params = np.polyfit(TE[..., self.gtab.b0s_mask],
                                np.log(data[..., self.gtab.b0s_mask]), te_order)
 
@@ -204,7 +174,30 @@ class Model(sfm.SparseFascicleModel):
             #    data[ii] = stats.scoreatpercentile(this_s0, 86)
             te_est = np.exp(np.polyval(te_params, this_te))
             data_no_te[ii] = data[ii] / te_est
+
+        AD = []
+        RD = []
+        bvals = []
+        for this_bval in np.unique(self.gtab.bvals[~self.gtab.b0s_mask]):
+            bval_idx = (self.gtab.bvals==this_bval)
+            this_gtab = grad.gradient_table(
+                np.hstack([self.gtab.bvals[self.gtab.b0s_mask],
+                           self.gtab.bvals[bval_idx]]),
+                np.vstack([self.gtab.bvecs[self.gtab.b0s_mask],
+                           self.gtab.bvecs[bval_idx]]))
             
+            tensor_model = dti.TensorModel(this_gtab)
+            tensor_fit = tensor_model.fit(
+                np.hstack([data_no_te[self.gtab.b0s_mask],
+                           data_no_te[bval_idx]]))
+
+            bvals.append(this_bval)
+            AD.append(tensor_fit.ad)
+            RD.append(tensor_fit.rd)
+
+        self.p_ad = np.polyfit(bvals, np.log(AD), 2)
+        self.p_rd = np.polyfit(bvals, np.log(RD), 2)
+
         sf_fit = sfm.SparseFascicleModel.fit(self, data_no_te, mask)
         return Fit(sf_fit, te_params)
         
@@ -239,14 +232,8 @@ class Fit(sfm.SparseFascicleFit):
         pred_sig : ndarray
             The signal predicted in each voxel/direction
         """
-        if gtab is None:
-            _matrix = self.model.design_matrix
-            gtab = self.model.gtab
-        # The only thing we can't change at this point is the sphere we use
-        # (which sets the width of our design matrix):
-        else:
-            _matrix = sfm_design_matrix(gtab, self.model.sphere, responses)
-            #_matrix = shore_design_matrix(40, 2000, gtab)
+        _matrix = sfm_design_matrix(gtab, self.model.sphere, responses,
+                                    self.model.p_ad, self.model.p_rd)
         # Get them all at once:
         beta_all = self.beta.reshape(-1, self.beta.shape[-1])
         pred_weighted = np.dot(_matrix, beta_all.T).T
