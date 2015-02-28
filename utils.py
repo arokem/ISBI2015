bbc7d9d68fd91bed01cf993bc728547c1327682c
cpoetter <cpoetter@stanford.edu>
2015-02-27 16:55:04 -0800

    Splitting Data in Seen & Unseen Randomly

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dipy.core.gradients as grad
 
 def b_value(g, delta, Delta, gamma=42.576):
     """ 
@@ -55,7 +56,55 @@ def LSE(prediction, signal, sigma=8):
 def RMSE(prediction, signal):
     return np.sqrt(np.mean((prediction - signal)**2))
 
+def create_shells(): 
+    # Dataset structure:
+    # -----------------
+    # 6 voxels a 3311 signals
+    # in total 33 shells 
+    # each shell consists of 2*45 antipodal DWI measurements
+    # a quadrupel of 3 bvals has the same TE
+    # this quadrupel hast 31 b0s
 
+    # Read data
+    data_dict = read_data()
+    bvals = data_dict['seen']['bvals']
+    bvecs = data_dict['seen']['bvecs']
+    delta = data_dict['seen']['delta']
+    Delta = data_dict['seen']['Delta']
+    te = data_dict['seen']['TE']
+    gtab = grad.gradient_table(bvals, bvecs, big_delta=Delta, small_delta=delta)
 
+    # Categorise data into Shells and TE Quadrupel
+    bvals_unique, shell  = np.unique(bvals, return_inverse=True)
+    te_unique, te_unique_mask  = np.unique(te, return_inverse=True)
 
+    # First entry for each quadrupel should be the b0s related to this TE
+    shell_mask = {}
+    for j in range(0, len(te_unique)):
+        shell_mask[j] = {}
+        shell_mask[j][0] = np.logical_and(te_unique_mask==j, gtab.b0s_mask)
 
+    te_counter = np.ones(len(te_unique)).astype(int)
+    for i in range(1, len(bvals_unique)): # for every shell
+        for j in range(0, len(te_unique)): # look at TE's
+            # and find the belonging one
+            if np.any(np.logical_and(te_unique_mask==j, shell==i) == True):
+                shell_mask[j][te_counter[j]] = shell==i
+                te_counter[j] += 1
+                
+    return shell_mask
+
+def split_data():
+    mask = create_shells()
+
+    # Pick random shells in TE quadrupel
+    unseen_shells = np.random.random_integers(1,3, len(mask))
+
+    # Create mask for unseen data
+    mask_unseen = np.zeros(mask[0][0].shape).astype(bool)
+    for i in range(0, len(mask)):
+        mask_unseen = np.logical_or(mask_unseen, mask[i][unseen_shells[i]])
+
+    mask_seen = np.invert(mask_unseen)
+    
+    return mask_seen, mask_unseen
